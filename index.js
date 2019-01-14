95e24c7218ac692ea63b4d1e4a9faa827ef62183
theschmocker <jacobcs1996@gmail.com>
2019-01-13 18:36:27 -0600

    replace imperative build process with SSG.build

--- index.js
+++ index.js
@@ -1,50 +1,14 @@
 const path = require('path');
-const siteData = require('./config.ssg.js').site;
 
-const { mkdir, readdir, access, constants: fsConstants } = require('./src/util/fs-promise');
-
-const Renderer = require('./src/Renderer');
-const Page = require('./src/Page');
+const SSG = require('./src/SSG');
 const PageWriter = require('./src/PageWriter');
+const Renderer = require('./src/Renderer');
 
-/**
- * Uses fs.access to determine whether or not a file exists
- * @param  {string} fileName - the filename (or path) to be checked for existence
- * @return {Promise} A promise that resolves to true or false, depending on whether or not the file exists
- */
-async function exists(fileName) {
-    try {
-        await access(fileName, fsConstants.F_OK);
-        return true;
-    } catch (e) {
-        return false
-    }
-}
+const renderer = new Renderer(path.join(process.cwd(), "_templates"), {
+    autoescape: false
+})
+const pageWriter = new PageWriter(renderer);
 
-/**
- * Build the site
- * TODO: Split this up into smaller functions
- */
-async function buildSite() {
-    const markdownFiles = (await readdir('.')).filter(fileName => fileName.endsWith('.md'));
-    const pages = markdownFiles.map(file => new Page(file));
+const ssg = new SSG(pageWriter);
 
-    const topLevelMenu = pages.map(page => ({
-        title: page.data.title,
-        link: page.data.path,
-    }));
-    
-    const siteDirExists = await exists('./_site');
-    if (!siteDirExists) await mkdir('./_site');
-
-    const pageWriter = new PageWriter(new Renderer(path.join(process.cwd(), "_templates"), { 
-        autoescape: false 
-    }));
-
-    for (page of pages) {
-        page.injectSiteData({ menu: topLevelMenu, ...siteData });
-        pageWriter.write(page);
-    }
-}
-
-buildSite();
+ssg.build();
