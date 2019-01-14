19fc6281a2d97126c4b66c9e4890b91aa44a66ad
theschmocker <jacobcs1996@gmail.com>
2019-01-13 18:35:41 -0600

    create static site generator class

--- src/SSG.js
+++ src/SSG.js
@@ -1,11 +1,61 @@
+const path = require('path');
+const { mkdir, readdir } = require('./util/fs-promise');
+
+const fileExists = require('./util/fileExists');
+const siteData = require('../config.ssg.js').site;
+
+const Page = require('./Page');
+
 class SSG {
-    constructor() {
-        
+    constructor(pageWriter) {
+        this.siteDirectory = '_site';
+        this.pages = [];
+        this.topLevelMenu = [];
+        this.pageWriter = pageWriter;
     }
 
-    build() {
-        console.log('building');
-        process.exit();
+    async _createSiteDirIfNecessary() {
+        const fullPathToSiteDir = path.join(process.cwd(), this.siteDirectory)
+        const siteDirExists = await fileExists(fullPathToSiteDir);
+
+        if (!siteDirExists) await mkdir(fullPathToSiteDir);
+    }
+
+    async _createPages() {
+        const markdownFiles = (await readdir('.')).filter(fileName => fileName.endsWith('.md'));
+        this.pages = markdownFiles.map(file => new Page(file));
+    }
+
+    _createTopLevelMenu() {
+        this.topLevelMenu = this.pages.map(page => ({
+            title: page.data.title,
+            link: page.data.path,
+        }));
+    }
+
+    _injectPagesWithSiteData() {
+        const data = {
+            menu: this.topLevelMenu,
+            ...siteData
+        }
+
+        this.pages.forEach(page => {
+            page.injectSiteData(data);
+        });
+    }
+
+    async _writePages() {
+        for (const page of this.pages) {
+            this.pageWriter.write(page);
+        }
+    }
+
+    async build() {
+        await this._createSiteDirIfNecessary();
+        await this._createPages();
+        this._createTopLevelMenu()
+        this._injectPagesWithSiteData();
+        await this._writePages();
     }
 }
 
