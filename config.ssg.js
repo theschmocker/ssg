1c781b1a8d81c82773dfa06c67ebe9b4a427b4f2
theschmocker <jacobcs1996@gmail.com>
2019-01-24 20:12:32 -0600

    uncomment posts collection to test error handling when a collection does not exist, or is empty

--- config.ssg.js
+++ config.ssg.js
@@ -10,9 +10,9 @@ module.exports = {
             name: 'employees',
             output: true, //when output is true, collection will be rendered to HTML
         },
-        // {
-        //     name: 'posts',
-        //     output: true, //when output is true, collection will be rendered to HTML
-        // }
+        {
+            name: 'posts',
+            output: true, //when output is true, collection will be rendered to HTML
+        }
     ]
 }
